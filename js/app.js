83280bc1d22baffded73e480fbd821611f43d6b6
igustavo11 <gustavobrun1224@gmail.com>
2024-03-29 10:56:45 -0300

    none

---
+++ js/app.js
@@ -0,0 +1,158 @@
+$(document).ready(function(){
+    cardapio.eventos.init();
+
+})
+var cardapio = {};
+
+
+
+var CELULAR_EMPRESA = '5527998496489';
+
+cardapio.eventos = {
+
+    init : () =>{
+        cardapio.metodos.obterItensCardapio();
+        cardapio.metodos.carregarBotaoLigar();
+        cardapio.metodos.carregarBotaoReserva();
+       
+    }
+
+}
+cardapio.metodos ={
+    // lista de itens cardapio
+    obterItensCardapio:(categoria = 'cafe', vermais = false ) =>{
+        var filtro = MENU[categoria];
+        console.log(filtro)
+
+        if(!vermais) {
+            $("#itensCardapio").html('');
+            $("#btnVerMais").removeClass('hidden');
+
+        }
+
+
+        $.each(filtro, (i, e)=>{
+            
+            let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
+            .replace(/\${name}/g, e.name)
+            .replace(/\${price}/g, e.price.toFixed(2).replace('.',','))
+            .replace(/\${id}/g, e.id)
+            // botao ver mais clicado 12 itens
+
+            if(vermais && i >= 3 && i < 12){
+                $("#itensCardapio").append(temp)
+
+            }
+            // pag inicial 8 itens
+            if(!vermais && i < 3) {
+                $("#itensCardapio").append(temp)
+
+            }
+            
+          
+
+        })
+
+        //remover active
+        $(".container-menu a").removeClass('active');
+        //menu ativo
+        $("#menu-"+ categoria).addClass('active')
+
+    },
+    //botao ver mais
+    verMais : () => {
+
+        var ativo =  $(".container-menu a.active").attr('id').split('menu-')[1];
+        cardapio.metodos.obterItensCardapio(ativo, true);
+
+        $("#btnVerMais").addClass('hidden');
+
+    },
+   
+
+    carregarBotaoReserva : ()=>{
+        var texto = 'Olá gostaria de fazer uma *reserva*';
+
+
+        let encode = encodeURI(texto);
+        let URL = ` https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+        $("#btnReserva").attr('href', URL);
+    },
+
+    carregarBotaoLigar:()=>{
+        $("#btnLigar").attr('href', `tel:${CELULAR_EMPRESA}`);
+    },
+
+//abre depoimentos dinamicamente
+    abrirDepoimento: (depoimento)=>{
+
+        $("#depoimento-1").addClass('hidden');
+        $("#depoimento-2").addClass('hidden');
+        $("#depoimento-3").addClass('hidden');
+
+        $("#btnDepoimento-1").removeClass('active');
+        $("#btnDepoimento-2").removeClass('active');
+        $("#btnDepoimento-3").removeClass('active');
+
+        $("#depoimento-" + depoimento).removeClass('hidden');
+        $("#btnDepoimento-" + depoimento).addClass('active');
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //mensagens
+    mensagem :(texto, cor = 'red', tempo = 3500)=> {
+
+        let id = Math.floor(Date.now() * Math.random()).toString();
+        
+       let msg = `<div id="msg-${id}" class ="animated fadeInDown toast ${cor}" >${texto}</div>`;
+       $("#container-mensagens").append(msg);
+
+       setTimeout(()=> {
+            $("#msg-"+ id).removeClass('fadeInDown');
+            $("#msg-"+ id).addClass('fadeOutUp');
+            setTimeout(() => {
+                $("#msg-"+ id).remove();
+
+                }, 800);
+          
+       }, tempo)
+
+    }
+
+}
+cardapio.templates = {
+    item: ` 
+            <div class="col-12 col-lg-3 col-md-3 col-sm-6  mb-5 animated fadeInUp">
+            <div class="card card-item" id="\${id}">
+                <div class="img-produto">
+                    <img src="\${img}" alt="">
+                </div>
+                <p class="title-produto text-center mt-4">
+                    <b>\${name}</b>
+                </p>
+                <p class="price-produto text-center">
+                    <b>R$\${price}</b>
+                </p>
+                <div class="add-carrinho">
+                   
+                </div>
+            </div>
+        </div> `,
+     
+}
---
+++ js/dados.js
@@ -0,0 +1,326 @@
+var MENU = {
+
+    "cafe":[
+        {
+            "id": "Prensa-Francesa",
+            "img": "./img/cardapio/cafe/Prensa-Francesa.png",
+            "name": "Prensa Francesa",
+            "dsc": "prensa cafe",
+            "price": 12.00
+
+        },
+        {
+            "id": "coado",
+            "img": "./img/cardapio/cafe/coado.png",
+            "name": "Coado",
+            "dsc": "coado",
+            "price": 7.00
+
+        },
+        {
+            "id": "expresso",
+            "img": "./img/cardapio/cafe/expresso.png",
+            "name": "Expresso",
+            "dsc": "expresso",
+            "price": 8.00
+
+        }
+
+    ],
+    "cappuccinos":[
+        {
+            "id": "Capuccino-Be-Coffee",
+            "img": "./img/cardapio/cappuccinos/Capuccino-Be-Coffee.png",
+            "name": "Capuccino Be Coffee",
+            "dsc": "cappuccino be",
+            "price": 17.00
+
+        },
+        {
+            "id": "Capuccino-doce-de leite",
+            "img": "./img/cardapio/cappuccinos/Capuccino-doce-de leite.png",
+            "name": "Capuccino doce de leite",
+            "dsc": "cappuccino doce ",
+            "price": 15.00
+
+        },
+        {
+            "id": "Capuccino-tradicional",
+            "img": "./img/cardapio/cappuccinos/Capuccino-tradicional.png",
+            "name": "Capuccino tradicional",
+            "dsc": "cappuccino tradicional",
+            "price": 13.00
+
+        },
+        {
+            "id": "Capuccino-italiano",
+            "img": "./img/cardapio/cappuccinos/cappuccino-italiano.png",
+            "name": "Capuccino italiano",
+            "dsc": "cappuccino italiano",
+            "price": 12.00
+
+        }
+        
+
+    ],
+    "chocolate quente":[
+        {
+            "id": "Chocolate-Quente",
+            "img": "./img/cardapio/chocolate quente/Chocolate-Quente.png",
+            "name": "Chocolate Quente",
+            "dsc": "chocolate quente",
+            "price": 15.00
+        }
+
+    ],
+    "bebidas geladas":[
+        {
+            "id": "Cappuccino-Gelado",
+            "img": "./img/cardapio/bebidas geladas/Cappuccino-Gelado.png",
+            "name": "Cappuccino Gelado",
+            "dsc": "Cappuccino-Gelado",
+            "price": 16.00
+        },
+        {
+            "id": "Frapê",
+            "img": "./img/cardapio/bebidas geladas/Frapê.png",
+            "name": "Frapê",
+            "dsc": "Frapê",
+            "price": 23.00
+        },
+        {
+            "id": "Soda-italiana",
+            "img": "./img/cardapio/bebidas geladas/Soda-italiana.png",
+            "name": "Soda italiana",
+            "dsc": "Soda italiana",
+            "price": 15.00
+            
+        }
+    ],
+    "toasts":[
+        {
+            "id": "toasts",
+            "img": " ",
+            "name": "Toast na Chapa",
+            "dsc": "toast",
+            "price": 5.00
+        },
+        {
+            "id": "toasts",
+            "img": "",
+            "name": "Queijo especial",
+            "dsc": "toasts",
+            "price": 8.00
+        },
+        {
+            "id": "toasts",
+            "img": "",
+            "name": "Ovos mexidos e bacon",
+            "dsc": "toasts",
+            "price": 12.00
+        }
+        
+    ],
+    "salgados":[
+        {
+            "id": "Camarao-Caravelas",
+            "img": "./img/cardapio/salgados/camaraocaravelas2.png",
+            "name": "Camarão Caravelas",
+            "dsc": "Camarao Caravelas",
+            "price": 14.00
+        },
+        {
+            "id": "Coxinha",
+            "img": "./img/cardapio/salgados/Coxinha.png",
+            "name": "Coxinha",
+            "dsc": "Coxinha",
+            "price": 10.00
+        },
+        {
+            "id": "Cuzcuz-de-milho",
+            "img": "./img/cardapio/salgados/Cuzcuz-de-milho.png",
+            "name": "Cuzcuz de milho",
+            "dsc": "Cuzcuz-de-milho",
+            "price": 13.00
+            
+        },
+        {
+            "id": "Empadao-de-Frango",
+            "img": "./img/cardapio/salgados/Empadao-de-Frango.png",
+            "name": "Empadão de Frango",
+            "dsc": "Empadao-de-Frango",
+            "price": 14.00
+        },
+        {
+            "id": "Porção-de-canudinho",
+            "img": "./img/cardapio/salgados/Porção-de-canudinho.png",
+            "name": "Porção de canudinho",
+            "dsc": "Porção-de-canudinho",
+            "price": 10.00
+        },
+        {
+            "id": "Quibe-artesanal",
+            "img": "./img/cardapio/salgados/Quibe-artesanal.png",
+            "name": "Quibe artesanal",
+            "dsc": "Quibe artesanal",
+            "price": 14.00
+        }
+        
+    ],
+    "tapiocas":[
+        {
+            "id": "Tapioca",
+            "img": "./img/cardapio/tapiocas/Tapioca.png",
+            "name": "Sem recheio/ Mantega",
+            "dsc": "tapioca",
+            "price": 13.00
+            
+        },
+        {
+            "id": "Tapioca",
+            "img": "./img/cardapio/tapiocas/Tapioca.png",
+            "name": "2 ingredientes",
+            "dsc": "tapioca",
+            "price": 14.00
+            
+        },
+        {
+            "id": "Tapioca",
+            "img": "./img/cardapio/tapiocas/Tapioca.png",
+            "name": "3 ingredientes(acrecimos consultar)",
+            "dsc": "tapioca",
+            "price": 15.00
+            
+        }
+
+        
+    ],
+    "tortas":[
+        {
+            "id": "Banoffee",
+            "img": "./img/cardapio/tortas/Banoffee.png",
+            "name": "Banoffee",
+            "dsc": "Banoffee",
+            "price": 18.00
+        },
+        {
+            "id": "Torta-de-limao",
+            "img": "./img/cardapio/tortas/Torta-de-limao.png",
+            "name": "Torta de limão",
+            "dsc": "Torta-de-limao",
+            "price": 18.00
+        },
+        {
+            "id": "Torta-holandesa",
+            "img": "./img/cardapio/tortas/Torta-holandesa.png",
+            "name": "Torta holandesa",
+            "dsc": "Torta-holandesa",
+            "price": 18.00
+        },
+
+        
+    ],
+    "bolos":[
+        {
+            "id": "Chocolatudo",
+            "img": "./img/cardapio/bolos/Chocolatudo.png",
+            "name": "Chocolatudo",
+            "dsc": "Chocolatudo",
+            "price": 18.00
+        },
+        {
+            "id": "Ninho-com-morango",
+            "img": "./img/cardapio/bolos/Ninho-com-morango.png",
+            "name": "Ninho com morango",
+            "dsc": "Ninho-com-morango",
+            "price": 18.00
+        },
+        {
+            "id": "Red-Velvet",
+            "img": "./img/cardapio/bolos/Red-Velvet.png",
+            "name": "Red Velvet",
+            "dsc": "Red-Velvet",
+            "price": 19.00
+        },
+
+        
+    ],
+    "Doces":[
+        {
+            "id": "Brownie-com-sorvete",
+            "img": "./img/cardapio/Doces/Brownie-com-sorvete.png",
+            "name": "Brownie com sorvete",
+            "dsc": "Brownie-com-sorvete",
+            "price": 19.00
+        },
+        {
+            "id": "Porção-mini-churros",
+            "img": "./img/cardapio/Doces/Porção-mini-churros.png",
+            "name": "Porção mini churros",
+            "dsc": "Porção-mini-churros",
+            "price": 22.00
+        },
+        {
+            "id": "banana-real",
+            "img": "./img/cardapio/Doces/banana-real2.png",
+            "name": "Banana real",
+            "dsc": "banana-real",
+            "price": 8.00
+        },
+        {
+            "id": "palha-italiana",
+            "img": "./img/cardapio/Doces/palha-italiana2.png",
+            "name": "Palha Italiana",
+            "dsc": "palha-italiana",
+            "price": 8.00
+        },
+        {
+            "id": "docinho",
+            "img": "./img/cardapio/Doces/docinho2.png",
+            "name": "Docinhos Gourmet",
+            "dsc": "docinho",
+            "price": 3.50
+        }
+        
+        
+
+      
+        
+    ],
+
+
+
+    
+    "bebidas": [
+        {
+            "id": "Agua-com-gas",
+            "img": "./img/cardapio/bebidas/Agua-com-gas.png",
+            "name": "Água com gás",
+            "dsc": "agua-com-gas",
+            "price": 5.00
+        },
+        {
+            "id": "Agua",
+            "img": "./img/cardapio/bebidas/Agua.png",
+            "name": "Água Mineral",
+            "dsc": "Agua",
+            "price": 4.00
+        },
+        {
+            "id": "Refrigerante-latas",
+            "img": "./img/cardapio/bebidas/Refrigerante-latas.png",
+            "name": "Refrigerante lata",
+            "dsc": "Refrigerante-latas",
+            "price": 6.00
+        },
+        {
+            "id": "suco",
+            "img": "./img/cardapio/bebidas/suco.png",
+            "name": "Sucos (consultar sabores)",
+            "dsc": "sucos",
+            "price": 7.00
+        }
+        
+    ],
+    
+}
